15ec1b91599e4a01010580938fdb8f86949cee94
Pritthijit Nath <41519676+nathzi1505@users.noreply.github.com>
2023-11-26 02:30:15 +0000

    Added Gualandi dataloader notebook

---
+++ utils/params.py
@@ -0,0 +1,175 @@
+"""
+Created on Wed Jan 11 10:46:23 2023
+
+@author: vinco
+"""
+
+
+def set_param(exp):
+    if exp == "b698":
+        parameters = {
+            "t0": 11100.0,
+            "tend": 11300.0,
+            "Nheaders": 2,
+            "dir_data": "gtc_quakes_data/labquakes/",
+            "case_study": "MeleVeeduetal2020/b698",
+            "data_type": "lab",
+            "struct_type": "MeleVeeduetal2020",
+            "file_format": "txt",
+            "downsample_factor": 1,
+            "vl": 10,
+            "segment": None,
+            "obs_unit": "MPa",
+            "time_unit": "s",
+        }
+        parameters["obs_label"] = r"$\tau_f$ [" + parameters["obs_unit"] + "]"
+        parameters["time_label"] = r"Time [" + parameters["time_unit"] + "]"
+    elif exp == "b726":
+        parameters = {
+            "t0": 9450.0,
+            "tend": 9650.0,
+            "Nheaders": 2,
+            "dir_data": "gtc_quakes_data/labquakes/",
+            "case_study": "MeleVeeduetal2020/b726",
+            "data_type": "lab",
+            "struct_type": "MeleVeeduetal2020",
+            "file_format": "txt",
+            "downsample_factor": 1,
+            "vl": 10,
+            "segment": None,
+            "obs_unit": "MPa",
+            "time_unit": "s",
+        }
+        parameters["obs_label"] = r"$\tau_f$ [" + parameters["obs_unit"] + "]"
+        parameters["time_label"] = (
+            r"$\tau_f$ [" + parameters["time_unit"] + "]"
+        )
+    elif exp == "i417":
+        parameters = {
+            "t0": 3650.0,
+            "tend": 3850.0,
+            "Nheaders": 2,
+            "dir_data": "gtc_quakes_data/labquakes/",
+            "case_study": "MeleVeeduetal2020/i417",
+            "data_type": "lab",
+            "struct_type": "MeleVeeduetal2020",
+            "file_format": "txt",
+            "downsample_factor": 1,
+            "vl": 10,
+            "segment": None,
+            "obs_unit": "MPa",
+            "time_unit": "s",
+        }
+        parameters["obs_label"] = r"$\tau_f$ [" + parameters["obs_unit"] + "]"
+        parameters["time_label"] = (
+            r"$\tau_f$ [" + parameters["time_unit"] + "]"
+        )
+    elif exp == "p4679":
+        parameters = {
+            "t0": 4233.28,  # 4229.0
+            "tend": 4535.0,
+            "Nheaders": 2,
+            "dir_data": "gtc_quakes_data/labquakes/",
+            "case_study": "Marone/p4679",
+            "data_type": "lab",
+            "struct_type": "Marone",
+            "file_format": "txt",
+            "downsample_factor": 1,
+            "vl": None,
+            "segment": None,
+            "obs_unit": "MPa",
+            "time_unit": "s",
+        }
+        parameters["obs_label"] = r"$\tau_f$ [" + parameters["obs_unit"] + "]"
+        parameters["time_label"] = r"Time [" + parameters["time_unit"] + "]"
+    elif exp == "p4581":
+        parameters = {
+            "t0": 2325.0,  # 2075.0,
+            "tend": 2525.0,  # 2275.0,
+            "Nheaders": 5,
+            "var4peaks": "ShearStress",
+            "peaks_dist": 300,
+            "peaks_height": 0.6,
+            "dir_data": "gtc_quakes_data/labquakes/",
+            "case_study": "Marone/p4581",
+            "struct_type": "Marone_p4581",
+            "data_type": "lab",
+            "file_format": "txt",
+            "vl": None,
+        }
+    elif exp == "sim_b698":
+        parameters = {
+            "t0": 0.0,
+            "tend": 200.0,
+            "Nheaders": 2,
+            "dir_data": "gtc_quakes_data/synquakes/",
+            "case_study": "Gualandietal2023/b698",
+            "data_type": "synthetic",
+            "struct_type": None,
+            "file_format": "txt",
+            "downsample_factor": 1,
+            "vl": None,
+            "segment": None,
+            "obs_unit": "MPa",
+            "time_unit": "s",
+        }
+        parameters["obs_label"] = r"$\tau_f$ [" + parameters["obs_unit"] + "]"
+        parameters["time_label"] = r"Time [" + parameters["time_unit"] + "]"
+    elif exp == "sim_b726":
+        parameters = {
+            "t0": 0.0,
+            "tend": 200.0,
+            "Nheaders": 2,
+            "dir_data": "gtc_quakes_data/synquakes/",
+            "case_study": "Gualandietal2023/b726",
+            "data_type": "synthetic",
+            "struct_type": None,
+            "file_format": "txt",
+            "downsample_factor": 1,
+            "vl": None,
+            "segment": None,
+            "obs_unit": "MPa",
+            "time_unit": "s",
+        }
+        parameters["obs_label"] = r"$\tau_f$ [" + parameters["obs_unit"] + "]"
+        parameters["time_label"] = r"Time [" + parameters["time_unit"] + "]"
+    elif exp == "sim_i417":
+        parameters = {
+            "t0": 0.0,
+            "tend": 200.0,
+            "Nheaders": 2,
+            "dir_data": "gtc_quakes_data/synquakes/",
+            "case_study": "Gualandietal2023/i417",
+            "data_type": "synthetic",
+            "struct_type": None,
+            "file_format": "txt",
+            "downsample_factor": 1,
+            "vl": None,
+            "segment": None,
+            "obs_unit": "MPa",
+            "time_unit": "s",
+        }
+        parameters["obs_label"] = r"$\tau_f$ [" + parameters["obs_unit"] + "]"
+        parameters["time_label"] = r"Time [" + parameters["time_unit"] + "]"
+    elif exp == "cascadia":
+        parameters = {
+            "t0": 2000.0,
+            "tend": 2030.0,
+            "Nheaders": None,
+            "dir_data": "gtc_quakes_data/slowquakes/",
+            "case_study": "Micheletal2019a/dynamical_system_variables_rough",
+            "data_type": "nature",
+            "struct_type": None,
+            "file_format": "mat",
+            "downsample_factor": 1,
+            "vl": 40e-3,
+            "segment": 1,
+            "obs_unit": r"m$^3$",
+            "time_unit": "yr",
+        }
+        parameters["obs_label"] = r"$P$ [" + parameters["obs_unit"] + "]"
+        parameters["time_label"] = r"Time [" + parameters["time_unit"] + "]"
+    else:
+        # pdb.set_trace()
+        print("`exp` not recognized.")
+    return parameters
